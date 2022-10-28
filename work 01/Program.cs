7d5cfe160a25e1cb07d185e95abcddf0cc34ec99
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-28 19:41:55 +0300

    Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.

---
+++ work 01/Program.cs
@@ -0,0 +1,57 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерного массива.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void OrderingArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1]) 
+                {
+                    int temp = 0;
+                    temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+    Console.WriteLine();
+    }        
+}
+
+Console.Write("Введите количество столбцов A: ");
+int A = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите количество строк B: ");
+int B = int.Parse(Console.ReadLine()?? "0");
+
+int[,] array = new int[A, B];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+OrderingArray(array);
+PrintArray(array);
